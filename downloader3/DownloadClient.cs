45dd0f4e85e7140e9379fca5108aabbba0bd69ce
pudingus <39903631+pudingus@users.noreply.github.com>
2017-02-22 17:26:25 +0100

    Implement Pause, Resume, file icon

--- downloader3/DownloadClient.cs
+++ downloader3/DownloadClient.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net;
@@ -19,41 +20,76 @@ namespace downloader3
         public long FileSize { get; private set; }
         public long BytesDownloaded { get; private set; }
         public int Index { get; set; }
-        public float Percentage { get; set; }
-
+        public float Percentage { get; private set; }
+        public long BytesPerSecond { get; private set; }
+        public double SecondsRemaining { get; private set; }
 
         private const int chunkSize = 1024;
         private string url;
         private string filename;
         private Thread downloadThread; 
-        private DispatcherTimer timer;
-        private bool doDownload = true;
+        private DispatcherTimer timer, timer2;
+        private bool cancel = false;
+        private bool pause = false;
+        private long persec;
+        private Stopwatch sw;     
+        
 
         public DownloadClient(string url, string filename)
         {
             this.url = url;
             this.filename = filename;
             timer = new DispatcherTimer();
+            timer2 = new DispatcherTimer();
+            sw = new Stopwatch();
         }
 
         public void Start()
         {
             timer.Tick += new EventHandler(Timer_Tick);
-            timer.Interval = new TimeSpan(0, 0, 0, 0, 100); //500 ms            
+            timer.Interval = new TimeSpan(0, 0, 0, 0, 500); //500 ms            
             timer.Start();
+
+            timer2.Tick += new EventHandler(Timer2_Tick);
+            timer2.Interval = new TimeSpan(0, 0, 1); //1 s            
+            timer2.Start();
+            sw.Start();
             downloadThread = new Thread(downloadWorker);
             downloadThread.Start();            
         }
 
-        public void Cancel()
-        {
-            doDownload = false;
-        }
-
         private void Timer_Tick(object sender, EventArgs e)
         {
-            OnDownloadProgressChanged(this);
+            if (BytesDownloaded > 0) SecondsRemaining = (FileSize - BytesDownloaded) * sw.Elapsed.TotalSeconds / BytesDownloaded;
+            OnDownloadProgressChanged(this);            
         }
+
+        private void Timer2_Tick(object sender, EventArgs e)
+        {
+            BytesPerSecond = persec;
+            persec = 0;
+        }
+
+        public void Cancel()
+        {
+            cancel = true;
+        }
+
+        public void Pause()
+        {
+            pause = true;
+            sw.Stop();
+            timer.Stop();
+            timer2.Stop();
+        }
+
+        public void Resume()
+        {
+            pause = false;
+            sw.Start();
+            timer.Start();
+            timer2.Start();
+        }        
 
         private void downloadWorker()
         {
@@ -68,17 +104,21 @@ namespace downloader3
                 byte[] read = new byte[chunkSize];
                 int count;
 
-                while ((count = receiveStream.Read(read, 0, chunkSize)) > 0 && doDownload) //dokud není přečten celý stream
+                while ((count = receiveStream.Read(read, 0, chunkSize)) > 0 && !cancel) //dokud není přečten celý stream
                 {
                     fs.Write(read, 0, count);                                    
 
                     BytesDownloaded += count;
+                    persec += count;
                     Percentage = ((float)BytesDownloaded / (float)FileSize) * 100;
+
+                    while (pause) Thread.Sleep(100);
                 }
                 fs.Close();
                 timer.Stop();
-                OnDownloadProgressCompleted(this, !doDownload);
-                if (!doDownload) File.Delete(filename);
+                sw.Reset();
+                OnDownloadProgressCompleted(this, cancel);
+                if (cancel) File.Delete(filename);
             }
         }
 
--- downloader3/MainWindow.xaml.cs
+++ downloader3/MainWindow.xaml.cs
@@ -19,6 +19,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.IO;
 using System.Windows.Threading;
+using System.Drawing;
 
 namespace downloader3
 {
@@ -28,8 +29,7 @@ namespace downloader3
     public partial class MainWindow : Window
     {              
         double speed;
-        int index;
-        Stopwatch sw = new Stopwatch();        
+        int index;              
         DownloadClient client;
 
         public MainWindow()
@@ -50,8 +50,12 @@ namespace downloader3
                 client.OnDownloadProgressCompleted += Client_OnDownloadProgressCompleted;
                 client.Index = index;
                 client.Start();
-                sw.Start();
-                DataView.Items.Add(new MyData() { Name = linksWindows.filename, Progress = 0, Client = client});
+                MyData item = new MyData();                
+                item.Name = System.IO.Path.GetFileName(linksWindows.filename);
+                item.Filename = linksWindows.filename;
+                item.Progress = 0;
+                item.Client = client;
+                DataView.Items.Add(item);                
                 index++;
             }            
         }
@@ -62,34 +66,34 @@ namespace downloader3
 
             MyData item = DataView.Items[c.Index] as MyData;
             item.Size = string.Format("{0} / {1}", ConvertFileSize(c.BytesDownloaded), ConvertFileSize(c.FileSize));
-            item.Progress = c.Percentage;
-            item.Percent = c.Percentage;
-            speed = c.BytesDownloaded / 1024 / sw.Elapsed.TotalSeconds;
+            item.Progress = c.Percentage;                      
+            speed = c.BytesPerSecond / 1024;
             item.Speed = string.Format("{0} kB/s", speed.ToString("0.0"));
-            item.Remaining = ConvertTime((c.FileSize - c.BytesDownloaded) * sw.Elapsed.TotalSeconds / c.BytesDownloaded);
+            item.Remaining = ConvertTime(c.SecondsRemaining);
+            var sysicon = System.Drawing.Icon.ExtractAssociatedIcon(item.Filename);
+            var bmpSrc = System.Windows.Interop.Imaging.CreateBitmapSourceFromHIcon(sysicon.Handle, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+            sysicon.Dispose();
+            item.Icon = bmpSrc;
 
-            DataView.Items.Refresh();
-
+            DataView.Items.Refresh();            
         }      
         
         private void Client_OnDownloadProgressCompleted(object sender, bool cancel)
         {
             Dispatcher.Invoke(delegate
             {
-                DownloadClient c = sender as DownloadClient;
-                sw.Reset();
+                DownloadClient c = sender as DownloadClient;                
                 MyData item = DataView.Items[c.Index] as MyData;
-
                 if (cancel)
                 {
                     item.Size = "";
-                    item.Progress = 0;
-                    item.Percent = 0;
+                    item.Progress = 0;               
                     item.Speed = "";
                     item.Remaining = "Zrušeno";                    
                 }
                 else
                 {
+                    item.Progress = 100;
                     item.Remaining = "Dokončeno";
                 }               
 
@@ -103,20 +107,20 @@ namespace downloader3
             double GB = Math.Pow(1024, 3);
             double TB = Math.Pow(1024, 4);
 
-            if (bytes >= TB) return string.Format("{0:0.00} TB", (bytes / TB));
-            else if (bytes >= GB) return string.Format("{0:0.00} GB", (bytes / GB));
-            else if (bytes >= 1) return string.Format("{0:0.00} MB", (bytes / MB));
+            if (bytes >= TB) return string.Format("{0:0.0} TB", (bytes / TB));
+            else if (bytes >= GB) return string.Format("{0:0.0} GB", (bytes / GB));
+            else if (bytes >= 1) return string.Format("{0:0.0} MB", (bytes / MB));
 
             else return "error";
         }        
 
-        private string ConvertTime(double dSeconds)
-        {   
+        private string ConvertTime(double seconds)
+        {
             string[] words = { "sekund", "minut", "hodin" };                        
             string str = " ";
-            int[] time = new int[3];
+            int[] time = new int[3];            
             
-            time[0] = Convert.ToInt32(dSeconds);
+            time[0] = Convert.ToInt32(seconds);
             time[1] = time[0] / 60;
             time[0] = time[0] - (time[1] * 60);
             time[2] = time[1] / 60;
@@ -141,7 +145,7 @@ namespace downloader3
             if (DataView.SelectedIndex == -1) return;
 
             MyData item = DataView.SelectedItem as MyData;
-            Process.Start(item.Name);         
+            Process.Start(item.Filename);         
         }
 
         private void MenuItem_Click_1(object sender, RoutedEventArgs e)
@@ -154,7 +158,7 @@ namespace downloader3
             ProcessStartInfo startInfo = new ProcessStartInfo();
 
             startInfo.FileName = "explorer.exe";
-            startInfo.Arguments = "/select, " + item.Name;
+            startInfo.Arguments = "/select, " + item.Filename;
             process.StartInfo = startInfo;
             process.Start();
         }
@@ -169,14 +173,42 @@ namespace downloader3
                 item.Client.Cancel();                
             }
         }
+
+        private void buttonPause_Click(object sender, RoutedEventArgs e)
+        {
+            MyData item = DataView.SelectedItem as MyData;
+            item.Client.Pause();
+            item.Remaining = "Pozastaveno";
+            item.Speed = "";
+            DataView.Items.Refresh();
+        }
+
+        private void buttonResume_Click(object sender, RoutedEventArgs e)
+        {
+            MyData item = DataView.SelectedItem as MyData;
+            item.Client.Resume();
+        }
+
+        private void buttonSettings_Click(object sender, RoutedEventArgs e)
+        {
+            SettingsWindow settingsWindow = new SettingsWindow();
+            settingsWindow.Owner = this;
+            settingsWindow.ShowDialog();
+        }
+
+        private void buttonLimit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 
     internal class MyData
     {
+        public ImageSource Icon { get; set; }
         public string Name { get; set; }
+        public string Filename { get; set; }
         public string Size { get; set; }
-        public float Progress { get; set; }
-        public float Percent { get; set; }
+        public float Progress { get; set; }        
         public string Speed { get; set; }
         public string Remaining { get; set; }
         public DownloadClient Client;
